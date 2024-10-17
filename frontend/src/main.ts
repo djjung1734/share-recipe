6828dd8074b3d6d7eb95eed74c16a00a19536208
djjung <ekwls8460@gmail.com>
2024-10-17 19:37:28 +0900

    axios 세팅 및 요청, 응답 인터셉터 추가

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,10 +1,64 @@
 import Vue from "vue";
 import App from "./App.vue";
 import { router } from "@/routers/index";
+import VueCookies from "vue-cookies";
+import store from "./store";
+import axios from "axios";
 
 Vue.config.productionTip = false;
+Vue.use(VueCookies);
+
+declare global {
+  interface Window {
+    axios: typeof axios;
+    $: any;
+  }
+}
+
+//axios 세팅
+window.axios = axios;
+window.axios.defaults.baseURL = `http://${window.location.hostname}:3000/api`;
+
+//axios 요청 interceptor
+window.axios.interceptors.request.use(
+  (config) => {
+    const authorization = config.headers.Authorization;
+    const accessToken = `Bearer ${Vue.$cookies.get("access_token")}`;
+    config.headers.Authorization = authorization ?? accessToken;
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+//axios 응답 interceptor
+window.axios.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const {
+      config,
+      response: { status },
+    } = error;
+    const refreshToken = Vue.$cookies.get("refresh_token");
+    if (status === 401 && config.url !== "/auth/refresh") {
+      await store.dispatch("auth", {
+        endpoint: "refresh",
+        options: {
+          headers: {
+            Authorization: `Bearer ${refreshToken}`,
+          },
+        },
+      });
+      config.headers.Authorization = `Bearer ${Vue.$cookies.get(
+        "access_token"
+      )}`;
+      return window.axios(config);
+    }
+    return Promise.reject(error);
+  }
+);
 
 new Vue({
-  render: (h) => h(App),
   router,
+  store,
+  render: (h) => h(App),
 }).$mount("#app");
