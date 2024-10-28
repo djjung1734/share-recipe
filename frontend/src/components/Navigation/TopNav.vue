2876a26e8ad143fd01fd0cd17592e2a26548081c
djjung <ekwls8460@gmail.com>
2024-10-28 17:02:43 +0900

    로그인 시 로그인버튼 닉네임으로 변경, 마이페이지 로그인 안했을 시 로그인 페이지로 이동

--- frontend/src/components/Navigation/TopNav.vue
+++ frontend/src/components/Navigation/TopNav.vue
@@ -15,19 +15,24 @@
         </button>
       </div>
       <div class="d-flex align-items-center">
-        <router-link to="/login">
-          <span
-            v-if="!user"
-            type="button"
-            class="me-3 text-white"
-            style="font-size: 12px">
+        <router-link v-if="!user" to="/login">
+          <span type="button" class="me-3 text-white" style="font-size: 12px">
             Log In
           </span>
-          <span v-else class="me-3 text-white" style="font-size: 12px">
-            {{ user.nickname }}
-          </span>
         </router-link>
-        <router-link to="/user/recipe">
+        <span v-else class="me-3 text-white" style="font-size: 12px">
+          {{ user.nickname }}
+        </span>
+        <router-link v-if="!user" to="/login">
+          <button type="button" class="btn border-0 rounded-circle">
+            <span
+              class="material-symbols-outlined text-white"
+              style="font-size: 32px">
+              person
+            </span>
+          </button>
+        </router-link>
+        <router-link v-else to="/user/recipe">
           <button type="button" class="btn border-0 rounded-circle">
             <span
               class="material-symbols-outlined text-white"
