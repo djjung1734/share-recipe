b027e88c89e2a9fde765713e8f2e551a71cba46f
djjung <ekwls8460@gmail.com>
2024-11-27 23:22:14 +0900

    레시피 get

--- frontend/src/components/ManagementRecipe.vue
+++ frontend/src/components/ManagementRecipe.vue
@@ -14,26 +14,24 @@
         </button>
       </div>
       <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 p-3">
-        <div class="col mb-5">
+        <div v-for="recipe in recipes" class="col mb-5">
           <div class="card h-100">
             <img
               class="card-img-top"
-              src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg"
+              :src="recipe.imagePath"
               alt="..."
+              width="200px"
+              height="133px"
             />
             <div class="card-body p-4">
               <div class="text-center">
                 <h5 class="fw-bolder">
-                  Special Item
+                  {{ recipe.title }}
                 </h5>
                 <div
                   class="d-flex justify-content-center small text-warning mb-2"
                 >
-                  <div class="bi-star-fill" />
-                  <div class="bi-star-fill" />
-                  <div class="bi-star-fill" />
-                  <div class="bi-star-fill" />
-                  <div class="bi-star-fill" />
+                  <div v-for="i in Number(recipe.level)" :key="i" class="bi-star-fill" />
                 </div>
                 <span class="text-muted text-decoration-line-through">
                   $20.00
@@ -41,11 +39,7 @@
                 $18.00
               </div>
             </div>
-            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-              <div class="text-center">
-                <a class="btn btn-outline-dark mt-auto" href="#">Add to cart</a>
-              </div>
-            </div>
+            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent" />
           </div>
         </div>
       </div>
