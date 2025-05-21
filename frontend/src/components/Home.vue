8bc22cb17b5d22561ae3dc3061439bb73064902d
djjung <ekwls8460@gmail.com>
2025-05-22 00:45:45 +0900

    메인화면 페이징 연동

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -7,7 +7,10 @@
           class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-5 p-3"
         >
           <div v-for="recipe in recipes" class="col mb-5">
-            <router-link :to="`/detail/${recipe.id}`" class="text-decoration-none">
+            <router-link
+              :to="`/detail/${recipe.id}`"
+              class="text-decoration-none"
+            >
               <div class="card h-100">
                 <img
                   class="card-img-top"
@@ -24,17 +27,38 @@
                     <div
                       class="d-flex justify-content-center small text-warning mb-2"
                     >
-                      <div v-for="i in Number(recipe.level)" :key="i" class="bi-star-fill" />
+                      <div
+                        v-for="i in Number(recipe.level)"
+                        :key="i"
+                        class="bi-star-fill"
+                      />
                     </div>
-                    <span class="text-muted">
-                      {{ recipe.time }}분 소요
-                    </span>
+                    <span class="text-muted"> {{ recipe.time }}분 소요 </span>
                   </div>
                 </div>
                 <div class="card-footer p-4 pt-0 border-top-0 bg-transparent" />
               </div>
             </router-link>
           </div>
+        </div>
+        <div class="d-flex justify-content-center align-items-center">
+          <button
+            v-if="pageNum > 1"
+            type="button"
+            class="btn"
+            @click="previousPage()"
+          >
+            <span class="material-symbols-outlined"> chevron_left </span>
+          </button>
+          <span class="pb-1">{{ pageNum }} / {{ lastPage }}</span>
+          <button
+            v-if="pageNum < lastPage"
+            type="button"
+            class="btn"
+            @click="nextPage()"
+          >
+            <span class="material-symbols-outlined"> chevron_right </span>
+          </button>
         </div>
       </div>
     </section>
@@ -49,9 +73,11 @@ export default Vue.extend({
   components: {
     TopNav,
   },
-  data():any {
+  data(): any {
     return {
       recipes: [],
+      pageNum: 1,
+      lastPage: null,
     };
   },
   mounted() {
@@ -59,9 +85,21 @@ export default Vue.extend({
   },
   methods: {
     loadRecipe() {
-      window.axios.get('/recipe').then((response) => {
-        this.recipes = response.data;
-      }).catch(() => null);
+      window.axios
+        .get('/recipe', { params: { pageNum: this.pageNum } })
+        .then((response) => {
+          this.recipes = response.data.data;
+          this.lastPage = response.data.lastPage;
+        })
+        .catch(() => null);
+    },
+    nextPage() {
+      this.pageNum++;
+      this.loadRecipe();
+    },
+    previousPage() {
+      this.pageNum--;
+      this.loadRecipe();
     },
   },
 });
--- server/src/recipe/recipe.service.ts
+++ server/src/recipe/recipe.service.ts
@@ -15,14 +15,13 @@ export class RecipeService {
 
   async findAll(pageNum: number, id?: number) {
     const whereCondition = id ? { userId: +id } : {};
-    const limit = 5;
+    const limit = 10;
     const [data, total] = await this.recipeRepository.findAndCount({
       where: whereCondition,
       relations: ['ingredients', 'steps', 'user'],
       skip: limit * (pageNum - 1),
       take: limit,
     });
-
     return { data, total, pageNum, lastPage: Math.ceil(total / limit) };
   }
 
